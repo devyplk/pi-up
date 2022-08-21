0c66f9cd9bd2d38d5566074046213310f27ffa97
devyplk <95547147+devyplk@users.noreply.github.com>
2022-08-21 21:05:13 +0530

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt upgrade -y
+mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.bak
+pihole -up
+mv /etc/lighttpd/lighttpd.conf.bak /etc/lighttpd/lighttpd.conf
+/etc/init.d/lighttpd restart
